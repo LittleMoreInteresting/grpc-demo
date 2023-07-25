5c962c591508076bb21321468e467abd147410e0
shr26207 <sunhaoran@offcn.com>
2023-07-25 09:18:20 +0800

    grpc-any

--- generate.go
+++ generate.go
@@ -1,7 +1,7 @@
 package main
 
 //hello
-//go:generate protoc -I=./hello/proto/ --go_out=./hello/ ./hello/proto/helloworld.proto  --go-grpc_out=./hello/
+//go:generate protoc -I=./hello/proto/ -I=./third_party/  --go_out=./hello/ ./hello/proto/helloworld.proto  --go-grpc_out=./hello/
 
 //streaming
 //go:generate protoc -I=./streaming/pb/ --go_out=./streaming/ ./streaming/pb/streaming.proto  --go-grpc_out=./streaming/
--- hello/proto/generate_windows.go
+++ hello/proto/generate_windows.go
@@ -2,5 +2,5 @@
 
 // 生成 proto
 //go:generate protoc -I=. -I=../../third_party --go_out=paths=source_relative:./ ./*.proto
-
+//go:generate protoc -I=. -I=../../third_party --go_out=. --go_opt=paths=source_relative ./*.proto
 package proto
